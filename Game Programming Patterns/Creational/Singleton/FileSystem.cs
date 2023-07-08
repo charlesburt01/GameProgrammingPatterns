d60c6ece0950fa42154e0333600b9a36af3b2469
Charles Burt <ctf_exe@hotmail.com>
2023-07-08 13:42:46 +0100

    organising folders, adding singleton

---
+++ Game Programming Patterns/Creational/Singleton/FileSystem.cs
@@ -0,0 +1,28 @@
+﻿class FileSystem
+{
+    private FileSystem(){} // closing off instantiation, by making the constructor private
+
+    private static FileSystem _instance;
+    public static FileSystem Instance 
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new FileSystem();
+            }
+            return _instance;
+        }
+    }
+
+    public Object File;
+}
+
+class MyClass
+{
+    void MyMethod()
+    {
+        var f = FileSystem.Instance.File;
+
+    }
+}
